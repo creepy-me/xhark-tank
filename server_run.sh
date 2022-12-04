39477afea0c56fac14d44e214ce8cce94583181d
Shraddha Gupta <shraddhagupta2319@gmail.com>
2022-12-04 18:58:53 +0530

    ME_BUILDOUT_XHARKTANK_MODULE_BASIC

--- server_run.sh
+++ server_run.sh
@@ -3,7 +3,7 @@
 # This script must contain a series of commands to run the backend application
 
 # # Eg:- For Node.js project;
-       nodemon index.js
+       node index.js
 
 # # Eg:- For SpringBoot Project;
 #         ./gradlew bootRun
